d7b5edc2a59fb97cd14d704413e9a3f92c42438d
rider0211 <codemaster9428@gmail.com>
2024-04-19 08:22:05 -0400

    update for database setting

--- otisproject/settings.py
+++ otisproject/settings.py
@@ -104,6 +104,9 @@ DATABASES = {
         'PASSWORD': 'Korgi123!',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
